b07240c55f75227318aac0f81f4948ad309bf3c7
AugustoReategui <carlosreategui1991@gmail.com>
2021-10-11 21:53:22 -0300

    finished_variables_and_operators exercises

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,25 @@
+// 1) Variables y Operadores
+
+// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var num1, num2, num3;
+num1 = 2;
+num2 = 5;
+sum = num1 + num2;
+console.log(sum);
+
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var firstName, lastName, fullName;
+firstName = "Augusto";
+lastName = "Reategui";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+
+var text1, text2, textLength;
+text1 = "New";
+text2 = "Car";
+textLength = text1.length + text2.length;
+console.log(textLength)
